700d5c417391803675997b0ce421465c3694d810
fv4jhro <karnal02@gmail.com>
2017-06-12 18:51:13 +0200

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+    <h2>TEST</h2>
+    <div v-for="record in usuarios">
+      <div>Prueba</div>
+    </div>
+  </div>
+</template>
+
+<script>
+var apiURL = 'https://randomuser.me/api/?results=25'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Prueba de los templates',
+      usuarios: []
+    }
+  },
+  ready: function () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData: function () {
+      this.$http.get(apiURL, function (v1users) {
+        console.log(v1users)
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
